57bc6a30f24f212aa8346c1a0dd3c2f7d1295661
Noah Schroeder <noahdschroeder@gmail.com>
2020-04-11 00:31:03 -0500

    Added final alien blow up sound

--- SpaceInvaders/SpaceInvaders/Player/MainShip.cs
+++ SpaceInvaders/SpaceInvaders/Player/MainShip.cs
@@ -6,18 +6,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace SpaceInvaders.Player
 {
     public class MainShip : MovableObject
     {
+        private MediaPlayer sounds = new MediaPlayer();
       public int Lives { get; private set; }
 
       public MainShip(int x, int y, List<Bitmap> image, List<Bitmap> deathanimation,int panelWidth, int panelHeight) : base(x, y, image, deathanimation, panelWidth, panelHeight)
       {
             Image = image;
             Lives = 3;
-      }
+            sounds.Open(new Uri(Util.bingPathToAppDir("Resources\\explosion.wav")));
+        }
       public override void Move(int deltaX, int deltaY)
       {
             X += deltaX;
@@ -41,6 +44,8 @@ namespace SpaceInvaders.Player
 
       public void Hit()
       {
+         sounds.Stop();
+         sounds.Play();
          Lives--;
          if (Lives <= 0)
          {
